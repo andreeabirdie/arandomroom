c5f22e68c3e34b7d9ae43728000e2b72357abbca
Andreea Vrabie <56505979+andreeabirdie@users.noreply.github.com>
2024-10-25 14:01:41 +0200

    Added material3 theming (palette + typography) and enabled edge to edge

--- composeApp/src/commonMain/kotlin/com/kmp/coachme/App.kt
+++ composeApp/src/commonMain/kotlin/com/kmp/coachme/App.kt
@@ -3,10 +3,14 @@ package com.kmp.coachme
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,21 +19,33 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import coachme.composeapp.generated.resources.Res
 import coachme.composeapp.generated.resources.compose_multiplatform
+import com.kmp.coachme.ui.theme.AppTheme
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+    var showContent by remember { mutableStateOf(false) }
+    AppTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                Modifier.fillMaxWidth().windowInsetsPadding(WindowInsets.safeDrawing),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    onClick = { showContent = !showContent }) {
+                    Text("Click me!")
+                }
+                AnimatedVisibility(showContent) {
+                    val greeting = remember { Greeting().greet() }
+                    Column(
+                        Modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(painterResource(Res.drawable.compose_multiplatform), null)
+                        Text("Compose: $greeting")
+                    }
                 }
             }
         }
